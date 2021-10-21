2a824723ce4c704b11acafe66e27fe8d4b77ed25
elhackeado <aman.thakur0001@hotmail.com>
2021-10-21 19:41:21 +0530

    updated comments

--- src/tests/test_3_listusers.py
+++ src/tests/test_3_listusers.py
@@ -1,25 +1,21 @@
-# test cases in this file are related to "/users" endpoint with GET method
-# this endpoint responds with list of all users present in the database
-#TESTS
-# 1. check response code
-# 2. check response content type
-# 3. check reponse message 
+# test cases in this file are related to "/users" URI with GET method
+# this URI responds with list of all users present in the database
 
 
 # importing json library to convert response data to json type
 import json
 
-# tests if correct response code is returned
+# tests if correct response code is returned after getting all users
 def test_getusers_respcode(app, client):
     res = client.get('/users')
     assert res.status_code == 200
 
-# tests if type of response content is json
+# tests if type of response content is json after getting all users
 def test_getusers_contenttype(app, client):
     res = client.get('/users')
     assert "application/json" == res.content_type
 
-# tests if correct data is displayed
+# tests if correct data is displayed after getting all users
 def test_getusers_content(app, client):
     res = client.get('/users')
     assert "totalUsers" in json.loads(res.get_data(as_text=True))
