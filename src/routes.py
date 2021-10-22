e9e50ed52c3f31fcafc4372dbfdac06d70e8bb69
elhackeado <aman.thakur0001@hotmail.com>
2021-10-22 12:02:39 +0530

    implemented exception handling

--- src/routes.py
+++ src/routes.py
@@ -24,8 +24,9 @@ def welcome():
 #  REGISTER NEW USER API
 @api.route('/users',methods=['POST'])
 def register():
-    #get payload from post request
+    
     try:
+        #get payload from post request
         payload = request.get_json()
         username = payload.get("username")
         #Make sure user doesn't already exists in the system
@@ -44,12 +45,16 @@ def register():
             addUser(user)
 
             return jsonify({"message":"new user registered successfully"}),201
-    #catch exception        
+        # return message if user already exists    
+        return jsonify({"message":"user already exists"})
+    # return message server error if any exception occurs
     except Exception:
-        return jsonify({"message":"bad requet"}),400
+        jsonify({
+            "message":"Internal server error"
+            }),500
 
 
-    return jsonify({"message":"user already exists"})
+    
 
 
 # LIST ALL USERS API
@@ -62,87 +67,117 @@ def listUsers():
 # GET SINGLE USER DETAILS API
 @api.route('/users/<username>',methods=['GET'])
 def getUser(username):
-    #check if user exists and then get the details
-    if userExists(username):
-        return jsonify(getUserDetails(username))
-    
-    #return message if user doesn't exists
-    return jsonify({
-        "message":"user doesn't exists"
-    }),404
+    try:
+        #check if user exists and then get the details
+        if userExists(username):
+            return jsonify(getUserDetails(username))
+        
+        #return message if user doesn't exists
+        return jsonify({
+            "message":"user doesn't exists"
+            }),404
+    # return message server error if any exception occurs
+    except Exception:
+        jsonify({
+            "message":"Internal server error"
+            }),500
 
 
 # UPDATE SINGLE USER DETAILS API
 @api.route('/users/<username>',methods=['PUT'])
 def updateUser(username):
-    payload = request.get_json()
-    #check if user exists 
-    if userExists(username):
-        fname = payload.get("fname")
-        lname = payload.get("lname")
-        pincode = payload.get("pincode")
-        # update the existing user details with the new details
-        updateUserDetails(username,fname,lname,pincode)
-        return jsonify({"message":"details updated sucessfully"})
-    
-    #return message if user doesn't exists
-    return jsonify({
-        "message":"user doesn't exists"
-    }),404
+    try:
+        payload = request.get_json()
+        #check if user exists 
+        if userExists(username):
+            fname = payload.get("fname")
+            lname = payload.get("lname")
+            pincode = payload.get("pincode")
+            # update the existing user details with the new details
+            updateUserDetails(username,fname,lname,pincode)
+            return jsonify({"message":"details updated sucessfully"})
+        
+        #return message if user doesn't exists
+        return jsonify({
+            "message":"user doesn't exists"
+        }),404
+    # return message server error if any exception occurs
+    except Exception:
+        jsonify({
+            "message":"Internal server error"
+            }),500
 
 
 # DELETE SINGLE USER WITH GIVEN USERNAME API
 @api.route('/users/<username>',methods=['DELETE'])
 def removeUser(username):
-    #check if user exists 
-    if userExists(username):
-        # delete user
-        deleteUser(username)
-        return jsonify({"message":"user deleted sucessfully"})
-    
-    #return message if user doesn't exists
-    return jsonify({
-        "message":"user doesn't exists"
-    }),404
+    try:
+        #check if user exists 
+        if userExists(username):
+            # delete user
+            deleteUser(username)
+            return jsonify({"message":"user deleted sucessfully"})
+        
+        #return message if user doesn't exists
+        return jsonify({
+            "message":"user doesn't exists"
+        }),404
+    # return message server error if any exception occurs
+    except Exception:
+        jsonify({
+            "message":"Internal server error"
+            }),500
 
 
 # ACTIVATE SINGLE USER WITH GIVEN USERNAME API
 @api.route('/users/<username>/activate',methods=['PATCH'])
 def activate(username):
-    # check if user exists
-    if userExists(username):
-        # make sure user already not activated
-        if not activated(username):
-            # activate user
-            activateUser(username)
-            return jsonify({"message":"user activated sucessfully"})
-        else:
-            return jsonify({"message":"user already activated"})
-    
-    #return message if user doesn't exists
-    return jsonify({
-        "message":"user doesn't exists"
-    }),404
+    try:
+        # check if user exists
+        if userExists(username):
+            # make sure user already not activated
+            if not activated(username):
+                # activate user
+                activateUser(username)
+                return jsonify({"message":"user activated sucessfully"})
+            else:
+                return jsonify({"message":"user already activated"})
+        
+        #return message if user doesn't exists
+        return jsonify({
+            "message":"user doesn't exists"
+        }),404
+    # return message server error if any exception occurs
+    except Exception:
+        jsonify({
+            "message":"Internal server error"
+            }),500
 
 
 
 # DEACTIVATE USER API
 @api.route('/users/<username>/deactivate',methods=['PATCH'])
 def deactivate(username):
-    # check if user exists
-    if userExists(username):
-        # check if user is activated
-        if activated(username):
-            # deactivate user
-            deactivateUser(username)
-            return jsonify({"message":"user deactivated sucessfully"})
-        else:
-            return jsonify({"message":"user already deactivated"})
-    
-    #return message if user doesn't exists
-    return jsonify({
-        "message":"user doesn't exists"
-    }),404
+    try:
+        # check if user exists
+        if userExists(username):
+            # check if user is activated
+            if activated(username):
+                # deactivate user
+                deactivateUser(username)
+                return jsonify({"message":"user deactivated sucessfully"})
+            else:
+                return jsonify({"message":"user already deactivated"})
+        
+        #return message if user doesn't exists
+        return jsonify({
+            "message":"user doesn't exists"
+        }),404
+    # return message server error if any exception occurs
+    except Exception:
+        jsonify({
+            "message":"Internal server error"
+            }),500
 
 
 
